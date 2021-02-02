fa6901baf1b731caa5737fd327ae851885778efd
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2021-02-02 18:59:14 +0000

    Add harvest tests back in

--- ckanext/spatial/tests/conftest.py
+++ ckanext/spatial/tests/conftest.py
@@ -4,6 +4,9 @@ import pytest
 
 import ckanext.harvest.model as harvest_model
 
+# to prevent all tables from being deleted
+model.repo.tables_created_and_initialised = True
+
 
 @pytest.fixture
 def harvest_setup():
