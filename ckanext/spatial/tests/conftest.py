799df0fdcf96b248c697abed5699d9a0536cf4ec
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2021-03-03 11:53:37 +0000

    Update tests to use clean_postgis

--- ckanext/spatial/tests/conftest.py
+++ ckanext/spatial/tests/conftest.py
@@ -4,9 +4,6 @@ import pytest
 
 import ckanext.harvest.model as harvest_model
 
-# to prevent all tables from being deleted
-model.repo.tables_created_and_initialised = True
-
 
 @pytest.fixture
 def harvest_setup():
