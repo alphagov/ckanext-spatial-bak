6f2a346a7987835c84814824e266e6f2a480c33a
kentsanggds <ken.tsang@digital.cabinet-office.gov.uk>
2024-06-20 14:39:27 +0100

    Update conftest.py for missing fixtures

--- ckanext/spatial/tests/conftest.py
+++ ckanext/spatial/tests/conftest.py
@@ -2,9 +2,32 @@
 
 import pytest
 
+from ckan.model import Session
+from ckanext.spatial.model.package_extent import setup as spatial_db_setup
 import ckanext.harvest.model as harvest_model
 
+
+def _create_postgis_extension():
+    Session.execute("CREATE EXTENSION IF NOT EXISTS postgis")
+    Session.commit()
+
+
+def create_postgis_tables():
+    _create_postgis_extension()
+
+
+@pytest.fixture
+def clean_postgis():
+    Session.execute("DROP TABLE IF EXISTS package_extent")
+    Session.execute("DROP EXTENSION IF EXISTS postgis CASCADE")
+    Session.commit()
 
 @pytest.fixture
 def harvest_setup():
     harvest_model.setup()
+
+
+@pytest.fixture
+def spatial_setup():
+    create_postgis_tables()
+    spatial_db_setup()
